511da868465c5e268a30e7a414561b005b7cd84b
Piotr Kedziora <piotrkedziorapl@gmail.com>
2019-04-11 22:55:36 +0200

    change menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 $(function(){
     $('#hamburger').click(function(){
-        $('.menu').slideToggle()
-    });
-    $(window).resize(function(){
-        if ($(window).width()>768) $('.menu').removeAttr('style');
+        $('.menu').toggleClass('menu--active');
+        $('#hamburger').toggleClass('is-active');
     });
 })
