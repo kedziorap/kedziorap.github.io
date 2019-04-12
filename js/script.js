5ca28177158be4c6500a7b64187f8d974c25cd52
Piotr Kedziora <piotrkedziorapl@gmail.com>
2019-04-12 22:46:43 +0200

    add about, skills

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $(function(){
-    $('#hamburger').click(function(){
-        $('.menu').toggleClass('menu--active');
-        $('#hamburger').toggleClass('is-active');
-    });
+    $('#hamburger').click(toggleMenu);
+    $('.menu a').click(toggleMenu);
+    function toggleMenu() {
+            $('.menu').toggleClass('menu--active');
+            $('#hamburger').toggleClass('is-active');
+    }
+
 })
