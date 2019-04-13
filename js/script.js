9129c38ebd544df4b9bf89614a7319fa23f1564d
Piotr Kedziora <piotrkedziorapl@gmail.com>
2019-04-13 12:52:14 +0200

    add news

--- js/script.js
+++ js/script.js
@@ -5,5 +5,19 @@ $(function(){
             $('.menu').toggleClass('menu--active');
             $('#hamburger').toggleClass('is-active');
     }
-
+    var project = $('.portfolio__project');
+    project.click(function(){
+        var node = $(this).find('.to-modal').html();
+        createModal(node);
+    })
+    function removeModal() {
+        $('.modal').remove();
+    }
+    function createModal(html) {
+        var modal = '<div class="modal"><div class="modal__container"><div class="modal__inside"><span class="modal__close">X</span>' + html + '</div></div></div>';
+        $('.wrapper').append(modal);
+        $('.modal__close').click(function(){
+            $('.modal').remove();
+        })
+    }
 })
