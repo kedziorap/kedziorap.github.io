7b7f4bc81006b94d14b384df4162e7ab125fe4f5
Piotr Kedziora <piotrkedziorapl@gmail.com>
2019-04-14 13:03:41 +0200

    add smooth scrolling

--- js/script.js
+++ js/script.js
@@ -26,4 +26,30 @@ $(function(){
         $('.wrapper').append(modal);
         $('.modal__close').click(removeModal);
     }
+    $('a[href*="#"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 })
