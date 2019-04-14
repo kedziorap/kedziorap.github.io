5e1a883c0fab9c396f65977ce1f3e2493a53cdea
Piotr Kedziora <piotrkedziorapl@gmail.com>
2019-04-14 12:47:23 +0200

    Add all

--- js/script.js
+++ js/script.js
@@ -5,19 +5,25 @@ $(function(){
             $('.menu').toggleClass('menu--active');
             $('#hamburger').toggleClass('is-active');
     }
-    var project = $('.portfolio__project');
-    project.click(function(){
-        var node = $(this).find('.to-modal').html();
-        createModal(node);
+    var btnProject = $('.details');
+    btnProject.click(function(){
+        var project = $(this).closest('.portfolio__project');
+        var title = project.find('.portfolio__title').text();
+        var links = project.find('.icon-anchor');
+        var html = project.find('.to-modal').html();
+        createModal(title,html, links);
     })
     function removeModal() {
         $('.modal').remove();
     }
-    function createModal(html) {
-        var modal = '<div class="modal"><div class="modal__container"><div class="modal__inside"><span class="modal__close">X</span>' + html + '</div></div></div>';
+    function createModal(title,html, links) {
+        var anchors = '';
+        for (var i = 0; i < links.length; i++) {
+            if (links[i].attributes['data-name'].nodeValue === 'code') anchors+='<a href="'+ links[i].lastChild.attributes['href'].value  +'" target="_blank">Kod</a>';
+            if (links[i].attributes['data-name'].nodeValue === 'live') anchors+='<a href="'+ links[i].lastChild.attributes['href'].value  +'" target="_blank">Live</a>';
+        }
+        var modal = '<div class="modal"><div class="modal__container"><div class="modal__inside"><span class="modal__close">X</span><h3>' + title + '</h3>' + html + anchors + '</div></div></div>';
         $('.wrapper').append(modal);
-        $('.modal__close').click(function(){
-            $('.modal').remove();
-        })
+        $('.modal__close').click(removeModal);
     }
 })
